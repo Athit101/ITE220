ce8cd76e7353ee4fd0f2b0632a1a4c4b5d47f1a3
Athit <celestialwolf101@hotmail.com>
2016-02-14 15:56:42 +0700

    added Week 8

---
+++ Week 8/concert.php
@@ -0,0 +1,47 @@
+<link href='css/style.css' rel='stylesheet' type='text/css'/>
+<?php
+echo "<!DOCTYPE HTML>
+<html>
+<head>
+
+<title> Concert Reservation Website</title>
+<link href='css/style.php' rel='stylesheet' type='text/css'/>
+<style>
+body {
+background-image: url('images/background.jpg');
+}
+</style>
+</head>
+<body>
+<h1> Welcome to Concert Reservation</h1>
+ <div id='click'>
+<h2> Book your Ticket  Now! </h2>
+ ";
+
+$seats = ['A-1', 'A-2', 'A-3', 'A-4', 'A-5', 
+      'B-1', 'B-2', 'B-3', 'B-4', 'B-5',
+      'C-1', 'C-2', 'C-3', 'C-4', 'C-5'];
+
+$i = 1;
+
+echo "<form action='concert_Result.php'
+   method='POST'>";
+
+    foreach ($seats as $seat) {
+             
+        echo "<input type='checkbox' name='seats[]' value='" .$seat. "'> </input>";
+        echo  "<span class='check'>" .$seat. "</span>            " ;
+       
+    	 if($i%5==0) {
+    		echo ("<br><br>");
+    	
+    }
+    ++$i;
+}
+
+echo "<input type ='submit' id='button' value='Buy Now' /></form>";
+echo "</div>
+</body>
+</html>";
+
+?>
